9881825f05b4d16f0aca2fea5101092ebd17549a
Dimitar Mundov <mundofff@gmail.com>
2023-01-16 00:16:15 +0200

    Adding Pagination

---
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductWithFiltersForCountSpecification : BaseSpecification<Product>
+    {
+        public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
+        :base(x=>
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
+        {
+        }
+    }
+}
