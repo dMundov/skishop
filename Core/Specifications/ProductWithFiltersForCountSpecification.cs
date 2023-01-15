f7641dd976c92d9fe941d5e74e0337048db80baf
Dimitar Mundov <mundofff@gmail.com>
2023-01-16 00:29:50 +0200

    Adding the search functionality

--- Core/Specifications/ProductWithFiltersForCountSpecification.cs
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -10,6 +10,7 @@ namespace Core.Specifications
     {
         public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
         :base(x=>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
                 (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
                 (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
         {
