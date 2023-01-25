f3137869c3a6169509164b7515a4bde5d7ecaaaf
Dimitar Mundov <mundofff@gmail.com>
2023-01-25 02:31:24 +0200

    Adding a Nav Bar component Creating a products interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'SkiShop';
+  products: IProduct[];
+  apiUrl= 'https://localhost:5001/api/products?pageSize=50'
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get(this.apiUrl).subscribe((response: IPagination)=> {
+      this.products= response.data;
+    }, error =>{
+      console.log(error);
+    });
+  }
+
 }
